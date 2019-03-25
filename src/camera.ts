9ed9cba47c98ba255f90bf64a63f2c8248f4af14
Jeroen Huinink <jeroen@studio-h3o.nl>
2019-03-25 16:27:55 +0100

    Cleanup camera.ts

--- src/camera.ts
+++ src/camera.ts
@@ -9,12 +9,15 @@ export interface ICamera {
   longitude: number;
 }
 
+// Read all lines from a file
 function readLines(fileName: string): Promise<string[]> {
   return new Promise((resolve, reject) => {
     fs.readFile(fileName, (err, data) => {
       if (err) {
         reject(err);
+        return;
       }
+
       if (!data) {
         resolve([]);
         return;
@@ -25,20 +28,23 @@ function readLines(fileName: string): Promise<string[]> {
   });
 }
 
+// Parse an array of strings (input lines)
 export function parseLines(lines: string[]) {
   return lines
-    .filter((l) => l && l !== "Camera;Latitude;Longitude")
-    .map(parse)
-    .filter((c) => c != null) as ICamera[];
+    .filter((l) => l && l !== "Camera;Latitude;Longitude") // skip empty lines and header line
+    .map(parse) // parse individual lines
+    .filter((c) => c != null) as ICamera[]; // remove any nulls due to errors
 }
 
+// Read all the camera information from the data file
 export async function readCameras() {
   return parseLines(await readLines("./data/cameras-defb.csv"));
 }
 
+// Search for a camera by name
 export function search(cameras: ICamera[], name: string) {
+  const searchString = name.toLocaleLowerCase();
   return cameras.filter(
-    (value) =>
-      value.name.toLocaleLowerCase().indexOf(name.toLocaleLowerCase()) > -1
+    (camera) => camera.name.toLocaleLowerCase().indexOf(searchString) > -1
   );
 }
