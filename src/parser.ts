44c0bcc0718daf9151be32e411deb1f11c1d4da6
Jeroen Huinink <jeroen@studio-h3o.nl>
2019-03-25 16:31:55 +0100

    Simplify parser.ts

--- src/parser.ts
+++ src/parser.ts
@@ -1,20 +1,38 @@
 import { ICamera } from "./camera";
 
+// Parse an individual line into a camera.
+// Parse errors are logged and a null is returned for such a line
 export function parse(line: string): ICamera | null {
+  // Split a csv line by the separator (;)
   const parts = line.split(";");
+
+  // check for parse errors
   if (!parts || parts.length !== 3) {
     console.error(`Parse error for: ${line}`);
     return null;
   }
-  const [name, latitude, longitude] = parts;
-  const regex = /^(.*)-(.*)-(\d+)[ -]?(.*)$/;
-  const matches = parts[0].match(regex);
 
+  // this yields the name, latitude and longitude
+  const [name, latitude, longitude] = parts;
+
+  // extract the id from the name with a regex
+  const regex = /^.*-.*-(\d+)[ -]?.*$/;
+  const matches = name.match(regex);
+
+  // check for parse errors
   if (!matches) {
     console.error(`Parse error for: ${line}`);
     return null;
-  } else {
-    const id = matches[3];
-    return { id: +id, name, latitude: +latitude, longitude: +longitude };
   }
+
+  // this yields the id
+  const id = matches[1];
+
+  // do so type conversions before we return the result
+  return {
+    id: +id,
+    name,
+    latitude: +latitude,
+    longitude: +longitude
+  };
 }
