a569cef862558e6083a0595e9993a0ca04e4ad49
Jeroen Huinink <jeroen@studio-h3o.nl>
2019-03-25 17:17:34 +0100

    Elaborated a bit on choices in code

--- code/index.ts
+++ code/index.ts
@@ -5,17 +5,26 @@ import { readCameras } from "./camera";
 async function start() {
   const cameras = await readCameras();
 
-  /* 
-  Doing the simplest thing that could possible work here.
-  On every request we will return the array of cameras in json format.
-  */
+  /*
+   * Doing the simplest thing that could possible work here.
+   * - `/api` returns the data,
+   * - everything else we will try to return a file from the code folder.
+   *
+   * This means that the sources are also exposed, but this is not about
+   * security and keeps things simple.
+   *
+   * Could have used express or something else to serve the pages and api,
+   * but now we have no external dependencies
+   */
   const server = http.createServer((req, res) => {
+    // Serve the data
     function serveApi() {
       res.setHeader("Content-Type", "application/json");
       res.write(JSON.stringify(cameras));
       res.end();
     }
 
+    // Serve files from ./code
     function servePage(url?: string) {
       const filename =
         !url || url === "/" ? "./code/index.html" : `./code${url}`;
@@ -36,12 +45,6 @@ async function start() {
         res.end();
       });
     }
-
-    // function serveError() {
-    //   res.writeHead(404);
-    //   res.end();
-    // }
-
     console.debug(req.url);
 
     if (req.url === "/api") {
